ed267ed98b5850cfa8b033ce79e8540af8c24460
DigiOhhh <37815914+DigiOhhh@users.noreply.github.com>
2018-07-16 23:59:12 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -186,11 +186,11 @@ int module_start(SceSize argc, const void *args)
         get_functions_retail_365();
         break;
 		
-    case 0x34B4D82E:
-        offsets[0] = 0x183F6C;
-        offsets[1] = 0x40E4FC;
-        get_functions_retail_367();
-        break;
+	case 0x34B4D82E:
+		offsets[0] = 0x183F6C;
+		offsets[1] = 0x40E4FC;
+		get_functions_retail_367();
+		break;
 
     case 0x12DAC0F3: // retail 3.68 SceShell
         offsets[0] = 0x183F6C;
