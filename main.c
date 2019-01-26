83377336a7bf5b581c4cca67fc9cc4f1cd8a7b1a
CelesteBlue-dev <celesteblue.dev@gmail.com>
2019-01-26 10:01:31 +0100

    Simplify code a bit (#15)

--- main.c
+++ main.c
@@ -55,12 +55,7 @@ static void get_functions_retail_360()
     scePafWidgetSetFontSize = (void*) text_addr + 0x45ce80;
 }
 
-static void get_functions_retail_365()
-{
-    scePafWidgetSetFontSize = (void*) text_addr + 0x45d2c8;
-}
-
-static void get_functions_retail_368()
+static void get_functions_retail_365_368()
 {
     scePafWidgetSetFontSize = (void*) text_addr + 0x45D2C8;
 }
@@ -186,16 +181,11 @@ int module_start(SceSize argc, const void *args)
         break;
 
     case 0x5549BF1F: // retail 3.65 SceShell
-        offsets[0] = 0x183f6c;
-        offsets[1] = 0x40e4fc;
-        get_functions_retail_365();
-        break;
-
     case 0x34B4D82E: // retail 3.67 SceShell
     case 0x12DAC0F3: // retail 3.68 SceShell
         offsets[0] = 0x183F6C;
         offsets[1] = 0x40E4FC;
-        get_functions_retail_368();
+        get_functions_retail_365_368();
         break;
 
     case 0xEAB89D5C: // PTEL 3.60 SceShell
